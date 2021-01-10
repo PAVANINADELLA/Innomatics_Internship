3d883bafb002177afbec7b04af80c6cce8305b04
PAVANI NADELLA <74247016+PAVANINADELLA@users.noreply.github.com>
2021-01-10 16:35:34 +0530

    Add files via upload

---
+++ TASKS - I - PORTFOLIO/templates/FLASK CSS.py
@@ -0,0 +1,49 @@
+<style>
+body
+{
+ margin: 0;
+ font-family: Arial, Helvetica, sans-serif;
+}
+
+p
+{
+ font-size: 14px;
+}
+
+.top-container
+{
+ background-color: #f1f1f1;
+ padding: 30px;
+ text-align: center;
+}
+
+.header
+{
+ padding: 1px 16px;
+ background: #555;
+ color: #f1f1f1;
+}
+
+.sticky
+{
+ position: fixed;
+ top: 0;
+ width: 100%;
+}
+
+.sticky + .content
+{
+ padding-top: 102px;
+}
+.footer
+{
+ position: fixed;
+ left: 0;
+ bottom: 0;
+ width: 100%;
+ background-color:#555 ;
+ color: #f1f1f1;
+ text-align: center;
+}
+</style>
+</head>
